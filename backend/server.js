416f8d3795cb29778922ece9c96335cfd4a1e995
Gatitosuave5 <N00234628@upn.pe>
2025-11-27 23:08:31 -0500

    first commit

---
+++ backend/server.js
@@ -0,0 +1,18 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/api", (req, res) => {
+  res.json({ mensaje: "API funcionando correctamente 😎" });
+});
+
+app.listen(3001, () => {
+  console.log("Servidor backend corriendo en http://localhost:3001");
+});
---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/* eslint-disable @typescript-eslint/no-require-imports */
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: [
+      "./app/**/*.{js,ts,jsx,tsx}",
+      "./components/**/*.{js,ts,jsx,tsx}",
+    ],
+    theme: {
+      extend: {},
+    },
+    plugins: [
+      require("tailwindcss-animate")
+    ],
+  };
